c67f21669b3f41947d076f3d04f3312c102c7d11
Alberto Merino Guerra <albmguer@gmail.com>
2024-07-11 16:33:36 +0100

    inyectar servicios en otros servicios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+
 import { CARS_SEED } from './data/cars.seed';
 import { BRANDS_SEED } from './data/brands.seed';
+import { BrandsService } from 'src/brands/brands.service';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService
+  ) {}
   
-  populateDB() {
-    //CARS_SEED
-    //BRANDS_SEED
+  populateDB() {    
+
+    this.carsService.fillCarsWithSeedData( CARS_SEED )
+    this.brandsService.fillBrandsWithSeedData( BRANDS_SEED)
 
     return 'Seed executed';
   }
